3f9b68832e2296433c0b29e3beb57390ac58f81c
sarafinataylor <125286254+sarafinataylor@users.noreply.github.com>
2023-02-13 09:06:20 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let sarafina= document.querySelector(".option-one");
+let diego= document.querySelector(".option-two");
+//two different first option screen
+let sarafinaDiv = document.querySelector(".option-one-screen");
+let diegoDiv = document.querySelector(".option-two-screen");
+//second button options
+
+//two different end option screens
+let sarafinainDiv = document.querySelector(".option-one-end");
+let diegoinDiv = document.querySelector(".option-two-end");
+//body
+let body = document.querySelector("body");
+
+
+
+
+sarafina.onclick=function(){
+sarafinaDiv.style.display="block";
+    sarafinaDiv.style.display="block";
+
+};
+
+diego.onclick=function(){
+diegoDiv.style.display="block"; diegoinDiv.style.display="block"; 
+};
+/*
+
+INSERT_VARIABLE.onclick=function(){
+
+};*/
